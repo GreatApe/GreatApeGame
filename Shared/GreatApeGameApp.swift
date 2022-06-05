300b643f068eb1ede20efa22032125b778cf402f
Gustaf Kugelberg <kugel@kth.se>
2022-06-05 23:31:03 +0200

    Initial Commit

---
+++ Shared/GreatApeGameApp.swift
@@ -0,0 +1,20 @@
+//
+//  GreatApeGameApp.swift
+//  Shared
+//
+//  Created by Gustaf Kugelberg on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GreatApeGameApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
