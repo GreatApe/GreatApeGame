56ceca4fc99f785fdd0faf61b0c91e08fc0f7218
Gustaf Kugelberg <kugel@kth.se>
2022-08-12 22:15:02 +0200

    App clip and ipad

---
+++ GreatApeAppClip/GreatApeAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatApeAppClipApp.swift
+//  GreatApeAppClip
+//
+//  Created by Gustaf Kugelberg on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GreatApeAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
